77a027e8a97e9927975dc697e6532214c0d11bf4
madhawaUCSC2453 <2020cs157@stu.ucsc.cmb.ac.lk>
2023-11-29 11:00:38 +0530

    git committed

--- my_stacked_app/lib/models/book.dart
+++ my_stacked_app/lib/models/book.dart
@@ -1,7 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:my_stacked_app/models/image_links.dart';
 part 'book.g.dart';
-part 'book.freezed.dart
+part 'book.freezed.dart';
 @freezed
 
 class  Book with _$Book {
